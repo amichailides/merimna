8bee11caabf571526887d886cc81ca42857f4704
amichailides <michailides.a@gmail.com>
2026-02-02 19:10:44 +0200

    chore: add main application entry point

---
+++ src/main/java/io/github/amichailides/merimna/MerimnaApplication.java
@@ -0,0 +1,13 @@
+package io.github.amichailides.merimna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerimnaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerimnaApplication.class, args);
+    }
+
+}
