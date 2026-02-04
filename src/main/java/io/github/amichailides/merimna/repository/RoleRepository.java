cbcd6e664ec9739758639a58ea4eefda980860a3
amichailides <michailides.a@gmail.com>
2026-02-04 13:30:50 +0200

    feat: add repositories for user and role persistence

--- src/main/java/io/github/amichailides/merimna/repository/RoleRepository.java
+++ src/main/java/io/github/amichailides/merimna/repository/RoleRepository.java
@@ -1,4 +1,14 @@
 package io.github.amichailides.merimna.repository;
 
-public interface RoleRepository {
+import io.github.amichailides.merimna.model.Role;
+import io.github.amichailides.merimna.model.UserRole;
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepositoryImplementation<Role, Long> {
+    Optional<Role> findByName(UserRole name);
+
 }
