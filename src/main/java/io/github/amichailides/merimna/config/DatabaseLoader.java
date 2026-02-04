64d21069b909399adf5fc67e8f868c4f7c4a66c7
amichailides <michailides.a@gmail.com>
2026-02-04 18:31:02 +0200

    feat: add databaseloader for automatic role seeding

--- src/main/java/io/github/amichailides/merimna/config/DatabaseLoader.java
+++ src/main/java/io/github/amichailides/merimna/config/DatabaseLoader.java
@@ -1,4 +1,32 @@
 package io.github.amichailides.merimna.config;
 
-public class DatabaseLoader {
+import io.github.amichailides.merimna.model.Role;
+import io.github.amichailides.merimna.model.UserRole;
+import io.github.amichailides.merimna.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+@RequiredArgsConstructor
+public class DatabaseLoader implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) {
+        Arrays.stream(UserRole.values()).forEach(roleEnum -> {
+            if (roleRepository.findByName(roleEnum).isEmpty()) {
+                Role newRole = Role.builder()
+                        .name(roleEnum)
+                        .description("Default system role for " + roleEnum)
+                        .build();
+
+                roleRepository.save(newRole);
+                System.out.println("generated role in db: " + roleEnum);
+            }
+        });
+    }
 }
