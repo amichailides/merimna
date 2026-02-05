1d69f160508e133344e9d67bfec0978a7e0f7dcd
amichailides <michailides.a@gmail.com>
2026-02-05 15:25:19 +0200

    feat: upgrade EmergencyContact to JPA embeddable with nested Address support

--- src/main/java/io/github/amichailides/merimna/model/EmergencyContact.java
+++ src/main/java/io/github/amichailides/merimna/model/EmergencyContact.java
@@ -1,19 +1,45 @@
 package io.github.amichailides.merimna.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
+@Embeddable
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class EmergencyContact {
+    @NonNull
+    @Column(nullable = false)
     private String firstName;
+
+    @NonNull
+    @Column(nullable = false)
     private String lastName;
+
+    @NonNull
+    @Column(nullable = false)
     private String relationship; // γονεας, δικαστικος συμπαραστατης κλπ
+
+    // TODO: Να προστεθεί Validation logic ώστε να είναι υποχρεωτικό τουλάχιστον ένα από τα δύο.
+    // Προς το παρόν παραμένουν προαιρετικά για διευκόλυνση του development.
     private String phoneNumber;
     private String mobileNumber;
     private String email;
+
+
+    /* * Σημείωση Logic: Χρησιμοποιούμε την Address ως Embedded.
+     * Αν υπάρχει επαφή, η διεύθυνση είναι επίσης υποχρεωτική για να ξέρουμε
+     * πού να αναζητήσουμε τον υπεύθυνο.
+     */
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "street", column = @Column(name = "contact_street")),
+            @AttributeOverride(name = "streetNumber", column = @Column(name = "contact_number")),
+            @AttributeOverride(name = "city", column = @Column(name = "contact_city")),
+            @AttributeOverride(name = "zipCode", column = @Column(name = "contact_zip"))
+    })
+    private Address address;
+
 }
