afe7c9ce4c1b889477411e337d91f2547f2df154
amichailides <michailides.a@gmail.com>
2026-02-06 21:23:44 +0200

    feat: add search and role filtering methods to UserRepository

--- src/main/java/io/github/amichailides/merimna/repository/UserRepository.java
+++ src/main/java/io/github/amichailides/merimna/repository/UserRepository.java
@@ -1,13 +1,22 @@
 package io.github.amichailides.merimna.repository;
 
+import io.github.amichailides.merimna.model.Role;
 import io.github.amichailides.merimna.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
+
+    List<User> findByLastNameContainingIgnoreCase(String lastName);
+
+    boolean existsByLastNameContainingIgnoreCase(String lastName);
+
+    List<User> findByRolesInAndActiveTrue(Collection<Role> roles);
 }
---
+++ src/test/java/io/github/amichailides/merimna/MerimnaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.amichailides.merimna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerimnaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
