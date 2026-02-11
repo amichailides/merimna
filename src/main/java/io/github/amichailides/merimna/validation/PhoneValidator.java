0b46cf1c623c012b88bf094e184d68a1c3cbe9b3
amichailides <michailides.a@gmail.com>
2026-02-11 20:47:02 +0200

    feat: add @ValidPhone custom annotation and validator for global reuse

---
+++ src/main/java/io/github/amichailides/merimna/validation/PhoneValidator.java
@@ -0,0 +1,14 @@
+package io.github.amichailides.merimna.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PhoneValidator implements ConstraintValidator<ValidPhone, String> {
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null || value.isBlank()) {
+            return true;
+        }
+
+        return value.matches(ValidationPatterns.PHONE) || value.matches(ValidationPatterns.MOBILE);
+    }
+}
