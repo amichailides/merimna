b4b74f3be7ed80084eaf6045317244d7ade26b20
amichailides <michailides.a@gmail.com>
2026-02-11 17:40:11 +0200

    feat: add @ValidFirstName and FirstNameValidator with Greek/Latin regex support

---
+++ src/main/java/io/github/amichailides/merimna/validation/FirstNameValidator.java
@@ -0,0 +1,13 @@
+package io.github.amichailides.merimna.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class FirstNameValidator implements ConstraintValidator<ValidFirstName, String> {
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null || value.isBlank()) {
+            return true;
+        }
+        return value.matches("^[A-Za-zΑ-Ωα-ωΆ-ώ\\s-]+$");
+    }
+}
---
+++ src/main/java/io/github/amichailides/merimna/validation/ValidFirstName.java
@@ -0,0 +1,23 @@
+package io.github.amichailides.merimna.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@NotBlank(message = "{name.required}")
+@Size(min=3, max=20, message = "{name.size}")
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = FirstNameValidator.class)
+public @interface ValidFirstName {
+    String message() default "{name.invalid}";
+
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
