46b6ccff354c31b2e7baf5bad3dab05bb03d33bc
amichailides <michailides.a@gmail.com>
2026-03-12 22:00:25 +0200

    fix: add missing repository methods for optimized retrieval

--- src/main/java/io/github/amichailides/merimna/repository/AllergyRepository.java
+++ src/main/java/io/github/amichailides/merimna/repository/AllergyRepository.java
@@ -4,5 +4,10 @@ import io.github.amichailides.merimna.model.Allergy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface AllergyRepository extends JpaRepository<Allergy, Long> {}
+public interface AllergyRepository extends JpaRepository<Allergy, Long> {
+
+    Optional<Allergy> findByIdAndBeneficiaryId(Long allergyId, Long beneficiaryId);
+}
