7f3e36a86aa2b08f856fd1aa6a24f67763030449
amichailides <michailides.a@gmail.com>
2026-03-20 21:11:58 +0200

    feat: add PageResponse wrapper DTO for paginated responses

---
+++ src/main/java/io/github/amichailides/merimna/common/PageResponse.java
@@ -0,0 +1,20 @@
+package io.github.amichailides.merimna.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResponse<T> {
+    private List<T> content;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+}
