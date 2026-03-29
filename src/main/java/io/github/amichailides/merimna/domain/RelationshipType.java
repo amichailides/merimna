4591a2062b7a7589bed85b1dfea884a15d59fa8e
amichailides <michailides.a@gmail.com>
2026-03-29 22:43:55 +0300

    chore: add TODO for future dynamic domain vocabularies (#11)

--- src/main/java/io/github/amichailides/merimna/domain/RelationshipType.java
+++ src/main/java/io/github/amichailides/merimna/domain/RelationshipType.java
@@ -1,5 +1,6 @@
 package io.github.amichailides.merimna.domain;
 
+// TODO(#11): Replace enum with entity for dynamic roles (multi-organization support)
 public enum RelationshipType {
     PARENT,
     SIBLING,
