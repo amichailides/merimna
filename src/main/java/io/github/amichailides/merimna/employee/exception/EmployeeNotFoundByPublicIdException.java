62adecf15479a8ffdf052475b5ad9dfcef0ce0f2
amichailides <michailides.a@gmail.com>
2026-05-08 12:42:00 +0300

    refactor(errors): move employee public id to context

--- src/main/java/io/github/amichailides/merimna/employee/exception/EmployeeNotFoundByPublicIdException.java
+++ src/main/java/io/github/amichailides/merimna/employee/exception/EmployeeNotFoundByPublicIdException.java
@@ -3,11 +3,13 @@ package io.github.amichailides.merimna.employee.exception;
 import io.github.amichailides.merimna.common.error.ErrorCode;
 import io.github.amichailides.merimna.exception.BaseApplicationException;
 
+import java.util.Map;
 import java.util.UUID;
 
 public class EmployeeNotFoundByPublicIdException extends BaseApplicationException {
-    public EmployeeNotFoundByPublicIdException(UUID publicId)
-    {
-        super(ErrorCode.EMPLOYEE_NOT_FOUND_BY_PUBLIC_ID, publicId);
+    public EmployeeNotFoundByPublicIdException(UUID publicId) {
+        super(ErrorCode.EMPLOYEE_NOT_FOUND_BY_PUBLIC_ID, Map.of(
+                "employeePublicId", publicId
+        ));
     }
 }
